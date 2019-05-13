9e677b6ee82a3f7d8327d40132c1919ea861e7d1
dede-kuntoro <50207351+dede-kuntoro@users.noreply.github.com>
2019-05-13 14:55:54 +0700

    Add files via upload

---
+++ love.sh
@@ -0,0 +1,56 @@
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+sleep 0.01
+cd
+clear
+sleep 0.01
+
+
+
+
+echo  " $green    Hai Sayang apa kabar!!!"
+
+
+
+
+
+
+
+
+
+
+cd $HOME/moni
+python love.py
+
+
+
+
+
+cd $HOME/Virus4/.tools
+sleep 0.01
+echo ""
+echo  "$red I Miss You"
+sleep 0.01
+
+echo ""
+figlet "Aku" | lolcat
+figlet "Sayang" | lolcat
+figlet "Kamu" | lolcat
+date | lolcat
+echo "IG: @dedekuntoro" | lolcat
+echo ""
+echo "JANGAN SAMPAI DRAMA DI SOSMED" | lolcat
+echo "MEMPENGARUHI PIKIRAN ANDA" | lolcat
+echo ""
+echo ""
+
